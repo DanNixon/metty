ead091a8fe1e03764ac05a895d49d463638ca2c5
Dan Nixon <dan@dan-nixon.com>
2026-01-20 17:21:47 +0000

    Switch to flake, add typos to CI, update dependabot config

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # Rust toolchain
+        rustup
+
+        # Code formatting tools
+        treefmt
+        alejandra
+        mdl
+        typos
+      ];
+    };
+  };
+}
